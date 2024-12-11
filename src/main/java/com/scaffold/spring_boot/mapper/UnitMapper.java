52109e73b67448e92e79bb852fa86f6b7f775d8d
PierceLe <haichau.hvsg@gmail.com>
2024-12-11 22:47:48 +0700

    chore: create unitResponse and using mapstruct with Unit

--- src/main/java/com/scaffold/spring_boot/mapper/UnitMapper.java
+++ src/main/java/com/scaffold/spring_boot/mapper/UnitMapper.java
@@ -2,6 +2,7 @@ package com.scaffold.spring_boot.mapper;
 
 import com.scaffold.spring_boot.dto.request.UnitCreationRequest;
 import com.scaffold.spring_boot.dto.response.UnitCreationResponse;
+import com.scaffold.spring_boot.dto.response.UnitResponse;
 import com.scaffold.spring_boot.entity.Unit;
 import org.mapstruct.Mapper;
 
@@ -9,4 +10,5 @@ import org.mapstruct.Mapper;
 public interface UnitMapper {
     Unit toUnit(UnitCreationRequest unitCreationRequest);
     UnitCreationResponse toUnitCreationResponse(Unit unit);
+    UnitResponse toUnitResponse(Unit unit);
 }
