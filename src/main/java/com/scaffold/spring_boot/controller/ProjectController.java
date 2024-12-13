bf48bdf38cbb649f46eafb54960b7d5c52f275a6
PierceLe <haichau.hvsg@gmail.com>
2024-12-13 13:20:15 +0700

    feat: create controller class for projects

---
+++ src/main/java/com/scaffold/spring_boot/controller/ProjectController.java
@@ -0,0 +1,15 @@
+package com.scaffold.spring_boot.controller;
+
+import com.scaffold.spring_boot.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+    private final ProjectService projectService;
+
+
+}
