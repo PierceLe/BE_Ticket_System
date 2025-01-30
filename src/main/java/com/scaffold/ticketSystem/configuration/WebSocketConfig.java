f7fbe4bb3ff83742e7e6b16fb9836fcea32d8b39
PierceLe <haichau.hvsg@gmail.com>
2025-01-31 00:14:11 +0700

    bug: fix bugs for websocket connection

--- src/main/java/com/scaffold/ticketSystem/configuration/WebSocketConfig.java
+++ src/main/java/com/scaffold/ticketSystem/configuration/WebSocketConfig.java
@@ -1,41 +1,43 @@
 package com.scaffold.ticketSystem.configuration;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.Setter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
+@Setter
 @Configuration
-@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Lazy
-    private final TaskScheduler messageBrokerTaskScheduler;
+    private TaskScheduler messageBrokerTaskScheduler;
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler(); // Provides a TaskScheduler bean
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")  // Fixed endpoint
-                .setAllowedOriginPatterns("*")  // Allow all origins for WebSocket
-                .withSockJS();  // Enable SockJS fallback
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        // STOMP messages whose destination header begins with /app are routed to
-        // @MessageMapping methods in @Controller classes
         config.setApplicationDestinationPrefixes("/app");
-        // Use the built-in message broker for subscriptions and broadcasting and
-        // route messages whose destination header begins with /topic or /queue to the broker
         config.enableSimpleBroker("/topic", "/queue")
                 .setHeartbeatValue(new long[]{10000, 20000})
-                .setTaskScheduler(this.messageBrokerTaskScheduler);
+                .setTaskScheduler(taskScheduler()); // Use the bean method instead of constructor injection
     }
 
     @Override
